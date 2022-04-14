fe39fc61a95cb63713500096662891715296e526
Ashwanth Kumar <kumaraswanth@gmail.com>
2022-04-14 15:30:25 +0530

    Add LSA implementation

--- Assignment 2/template_code_part2/informationRetrieval.py
+++ Assignment 2/template_code_part2/informationRetrieval.py
@@ -85,3 +85,61 @@ class InformationRetrieval():
 		print("Ranking complete in {} seconds".format(end-start))
 	
 		return doc_IDs_ordered
+
+	def rank_by_lsa(self, queries, k):
+		"""
+		Rank the documents according to relevance for each query using LSA
+
+		Parameters
+		----------
+		arg1 : list
+			A list of lists of lists where each sub-list is a query and
+			each sub-sub-list is a sentence of the query
+		
+		arg2 : number
+			k important features of LSA
+
+		Returns
+		-------
+		list
+			A list of lists of integers where the ith sub-list is a list of IDs
+			of documents in their predicted order of relevance to the ith query
+		"""
+
+		doc_IDs_ordered = []
+
+		start = time.time()
+		U, S, Vt = np.linalg.svd(self.index.toarray())
+		end = time.time()
+		print("Time to do SVD computation in {} seconds".format(end - start))
+
+		# Consider the top k features
+		Uk = U[:, :k]
+		Sk = np.diag(S[:k])
+		Vk = Vt[:k, :].T
+
+		self.tfidf_transformer.fit(self.term_doc_freq)
+		self.tfidf = self.tfidf_transformer.transform(self.term_doc_freq)
+
+		start = time.time()
+
+		query_sents = [' '.join(list(itertools.chain.from_iterable(x))) for x in queries]
+		query_counts = self.count_vectorizer.transform(query_sents)
+		query_tfidf = self.tfidf_transformer.transform(query_counts)
+
+		# get the query in latent space 
+		query_tfidf = (query_tfidf @ Uk) @ np.linalg.inv(Sk)
+
+		# compute cosine similarity
+		cos_sim = cosine_similarity(query_tfidf, Vk)
+
+		for cos_similarity_vector in cos_sim:
+			top_n_doc_indexes = cos_similarity_vector.argsort()[::-1]
+			# convert doc_indexes to docIDs
+			top_n_docs = [self.docIDs[doc_index] for doc_index in top_n_doc_indexes]
+			doc_IDs_ordered.append(top_n_docs)
+
+		end = time.time()
+		print("Ranking complete in {} seconds".format(end-start))
+	
+		return doc_IDs_ordered
